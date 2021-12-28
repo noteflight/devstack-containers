5f34080c1615599ef95e1b3008a1d2243e8d5d25
arista <arista@abramsons.info>
2021-12-28 16:21:11 -0500

    Removed the no-cache from build-local

--- build-local.sh
+++ build-local.sh
@@ -3,6 +3,6 @@
 # Get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build --no-cache -t nf-devstack-shell ${DIR}/containers/nf-devstack-shell
-docker build --no-cache -t nf-devstack-dynamodblocal ${DIR}/containers/nf-devstack-dynamodblocal
-docker build --no-cache -t nf-devstack-elasticsearch ${DIR}/containers/nf-devstack-elasticsearch
+docker build -t nf-devstack-shell ${DIR}/containers/nf-devstack-shell
+docker build -t nf-devstack-dynamodblocal ${DIR}/containers/nf-devstack-dynamodblocal
+docker build -t nf-devstack-elasticsearch ${DIR}/containers/nf-devstack-elasticsearch
