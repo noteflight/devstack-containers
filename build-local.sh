7423c4a42137408d88e022f2e78cb527cf96f5f3
arista <arista@abramsons.info>
2021-12-30 09:47:26 -0500

    added customization of nf-devstack-shell

--- build-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker build -t nf-devstack-shell ${DIR}/containers/nf-devstack-shell
-docker build -t nf-devstack-dynamodblocal ${DIR}/containers/nf-devstack-dynamodblocal
-docker build -t nf-devstack-elasticsearch ${DIR}/containers/nf-devstack-elasticsearch
